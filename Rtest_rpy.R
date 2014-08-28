8927e1f7d603d97023320d8e6951e2d4ec15ad3b
cotterman <cotterman@gmail.com>
2014-08-28 12:10:27 -0700

    added testing code for rpy2 interaction

---
+++ Rtest_rpy.R
@@ -0,0 +1,15 @@
+#############################################################################
+############### test code to work with python's rpy package  ################
+#############################################################################
+
+#R packages are not the only way to distribute code. From this author’s experience there exists R code circulating as .R files.
+
+#This is most likely not a good thing, but as a Python developers this also what you might be given with the task to implement an application 
+#(such a web service) around that code. 
+#In most working places you will not have the option to refuse the code until it is packaged; 
+#fortunately rpy2 is trying to make this situation as simple as possible.
+
+run_test_wrap = function(resp_from_python){
+  
+  
+}
